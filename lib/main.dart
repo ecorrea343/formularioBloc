16d68ae85165ca9671871f6988ef9fc3bc7e7622
Emmanuel Correa <ebcorreac@gmail.com>
2019-10-18 13:35:40 -0300

    Primer Commir de configuracion

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:formulariobloc/src/pages/login_page.dart';
+ 
+void main() {
+  
+  runApp(MyApp());
+  
+  }
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Formulario',
+      initialRoute: 'login',
+      routes: {
+        'login' : (BuildContext context) => LoginPage(),
+      },
+    );
+  }
+}
