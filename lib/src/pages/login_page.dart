d06a19e475f441495a78e012184ef6be49fd954c
Emmanuel Correa <ebcorreac@gmail.com>
2019-10-18 17:14:16 -0300

    terminado el diseño de la pantala de Login

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -9,7 +9,8 @@ class LoginPage extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           _crearFondo(context),
-
+          _crearCaja(context),
+          
         ],
       ),
       
@@ -54,7 +55,7 @@ class LoginPage extends StatelessWidget {
           padding: EdgeInsets.only(top: 80.0),
           child: Column(
             children: <Widget>[
-              Icon(Icons.person_pin_circle,size:100.0,color: Colors.white,),
+              Icon(Icons.person_pin_circle,size:80.0,color: Colors.white,),
               SizedBox(height:10.0,width: double.infinity,),
               Text('usuario Espol',style: TextStyle(color: Colors.white,fontSize: 25.0),)
             ],
@@ -63,6 +64,108 @@ class LoginPage extends StatelessWidget {
       ],
 
     );
+
+ }
+
+ Widget _crearCaja(BuildContext context){
+
+   final size = MediaQuery.of(context).size;
+
+   return SingleChildScrollView(
+     child: Column(
+       children: <Widget>[
+
+          SafeArea(
+            child: Container(
+              height: 180.0,
+            ),
+          ),
+
+         Container(
+           
+           width: size.width * 0.85,
+           margin: EdgeInsets.symmetric(vertical: 10.0),
+           padding: EdgeInsets.symmetric(vertical: 70.0),
+           decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5.0),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 7.0,
+                  offset: Offset(0.0,5.0),
+                  spreadRadius: 3.0
+                ),
+              ]
+            ),
+           child:Column(
+
+             children: <Widget>[
+               Text('Ingreso',style: TextStyle(fontSize: 20.0),),
+               SizedBox(height: 40.0,),
+               _crearEmail(),
+               SizedBox(height:10.0),
+               _crearPass(),
+               SizedBox(height:10.0),
+               _crearBoton(),
+               
+
+             ],
+           ) ,
+         ),
+         Text('Olvido su contraseña'),
+         SizedBox(height: 100.0)
+       ],
+     ),
+   );
+ }
+ Widget _crearEmail(){
+
+   return Container(
+     padding: EdgeInsets.symmetric(horizontal: 20.0),
+     child: TextField(
+       keyboardType: TextInputType.emailAddress,
+       decoration: InputDecoration(
+         icon: Icon(Icons.alternate_email, color:Colors.deepPurple),
+         hintText: 'example@espol.cl',
+         labelText: 'Correa Electronico'
+       ),
+     ),
+   );
+
+ }
+ Widget _crearPass(){
+
+   return Container(
+     padding: EdgeInsets.symmetric(horizontal: 20.0),
+     child: TextField(
+       keyboardType: TextInputType.visiblePassword,
+       obscureText: true,
+       decoration: InputDecoration(
+         icon: Icon(Icons.lock_outline, color:Colors.deepPurple),
+         hintText: 'example@espol.cl',
+         labelText: 'Contraseña'
+       ),
+     ),
+   );
+
+ }
+ Widget _crearBoton(){
+
+   return RaisedButton(
+     child: Container(
+       padding: EdgeInsets.symmetric(horizontal: 80.0,vertical: 15.0),
+       child: 
+         Text('Ingresar'),
+     ),
+     shape: RoundedRectangleBorder(
+       borderRadius: BorderRadius.circular(5.0)
+     ),
+     elevation: 0.0,
+     color: Colors.deepPurple,
+     textColor: Colors.white,
+     onPressed: (){},
+   );
 
  }
 
