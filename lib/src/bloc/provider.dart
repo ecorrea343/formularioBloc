76c4dd76c44e20ecb0434cc0537b61c84b186023
Emmanuel Correa <ebcorreac@gmail.com>
2019-10-18 17:43:30 -0300

    v2

---
+++ lib/src/bloc/provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:formulariobloc/src/bloc/login_bloc.dart';
+
+class Provider extends InheritedWidget{
+
+  final loginBloc =     LoginBloc();
+
+  Provider({Key key, Widget child})
+  : super (key : key,child:child);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) => true;
+
+  static LoginBloc of ( BuildContext context ) {
+
+    return ( context.inheritFromWidgetOfExactType(Provider) as Provider).loginBloc;
+
+  }
+}
